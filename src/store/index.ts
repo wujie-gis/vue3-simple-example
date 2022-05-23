64c1aa9d575f474dca23243ec1a5402253641ca5
wuj <1450626722@qq.com>
2022-05-23 17:00:54 +0800

    ✨ feat(): fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,9 @@
 import { createPinia } from 'pinia';
 import { useUserStore } from './modules/userStore';
+import piniaPluginPersist from 'pinia-plugin-persist';
 const store = createPinia();
 
+store.use(piniaPluginPersist);
 export default store;
+
 export { useUserStore };
